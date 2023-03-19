b50c3c6cc2e8b584c5df7443bdeac74be0e80d96
nkruger <nicolas.nkruger@gmail.com>
2023-03-19 15:27:47 -0300

    img alt

---
+++ src/app/img/page.tsx
@@ -0,0 +1,10 @@
+import { ComeBack } from "@/components/atom/comeback/comeback";
+
+export default function Img() {
+  return (
+    <div>
+      <ComeBack />
+      <img alt="description" src="naota.jpg" />
+    </div>
+  );
+}
---
+++ src/components/atom/comeback/comeback.tsx
@@ -0,0 +1,3 @@
+import Link from "next/link";
+
+export const ComeBack = () => <Link href={"/"}>Come back</Link>;
